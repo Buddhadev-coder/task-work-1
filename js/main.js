e36610f2d79afe50b52265e20b25fc2302746df7
Buddhadev-coder <sahoobuddhadev696@gmail.com>
2026-01-20 21:13:02 +0530

    second commit

--- js/main.js
+++ js/main.js
@@ -8,24 +8,24 @@ const swiper = new Swiper(".textSwiper", {
         // Mobile (default)
         0: {
             slidesPerView: 1.5,
-            spaceBetween: 15,
+            
         },
 
         // Tablet
         640: {
             slidesPerView: 2,
-            spaceBetween: 20,
+            
         },
 
         768: {
             slidesPerView: 3,
-            spaceBetween: 25,
+        
         },
 
         // Desktop
         1024: {
             slidesPerView: 4,
-            spaceBetween: 30,
+    
         },
     },
 });
